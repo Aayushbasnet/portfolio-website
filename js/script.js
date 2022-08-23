17419683aac25ae17f99211f6fd70100ce9b470d
Aayush Basnet <aayushbasnet07@gmail.com>
2022-08-23 14:27:01 +0545

    active button on navbar handled properly

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 // Active button on navbar
-
 var headerUl = document.querySelector(".headerUl");
 var liLists = headerUl.querySelectorAll("li");
 liLists.forEach(liList => {
+    var a = liList.querySelector("a");
+    if(liList.classList.contains("active")){
+        a.style.color = "#FFFFFF";
+    }
     liList.addEventListener("click", (e) =>{
         var current = document.querySelectorAll(".active");
-        current[0].classList.remove("active");       
-        liList.classList.add("active");
+        current[0].classList.remove("active");  // remove active class from old active btn
+        current[0].querySelector("a").style.color = "#383838"; // reset color
+        liList.classList.add("active"); // add active class for new active btn
+        liList.querySelector("a").style.color = "#FFFFFF";  // set color
         // console.log(liList);
     });
 });
